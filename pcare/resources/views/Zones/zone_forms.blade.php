1fa51ab01e7bca1d2b47003e2ad1f88d4722409a
w-graph-solutions let <ewave80@gmail.com>
2019-12-18 21:49:39 +0000

    initial commit

---
+++ pcare/resources/views/Zones/zone_forms.blade.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="mx-auto w-50">
+        <div class="d-flex">
+            <label for="name">Zone Name</label>
+            <input type="text" class="form-control" name="required">
+        </div>
+    </div>
+</div>
